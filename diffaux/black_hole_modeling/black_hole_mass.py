Fix errors in scaling relation

diffaux/black_hole_modeling/black_hole_mass.py
@@ -14,17 +14,22 @@ def bh_mass_from_bulge_mass(bulge_mass):
     Parameters
     ----------
     bulge_mass : ndarray
-        Numpy array of shape (ngals, ) storing the stellar mass of the bulge
-        in units of solar mass assuming h=0.7
+        Stellar mass of the bulge in units of Msun
 
     Returns
     -------
     bh_mass : ndarray
-        Numpy array of shape (ngals, ) storing black hole mass
+        Mass of the black hole in units of Msun
+
+    Notes
+    -----
+    See arXiv:1304.7762, section 6.1 for power-law exponent and normalization
 
     """
-    prefactor = 0.49 * (bulge_mass / 100.0)
-    return prefactor * (bulge_mass / 1e11) ** 0.15
+    prefactor = 0.0013
+    alpha = 1.0
+    bh_mass = prefactor * bulge_mass**alpha
+    return bh_mass
 
 
 def monte_carlo_black_hole_mass(bulge_mass, ran_key):
@@ -35,8 +40,7 @@ def monte_carlo_black_hole_mass(bulge_mass, ran_key):
     Parameters
     ----------
     bulge_mass : ndarray
-        Numpy array of shape (ngals, ) storing the stellar mass of the bulge
-        in units of Msun assuming h=0.7
+        Stellar mass of the bulge in units of Msun
 
     ran_key : jax.random.key
         Random number seed
@@ -44,12 +48,15 @@ def monte_carlo_black_hole_mass(bulge_mass, ran_key):
     Returns
     -------
     bh_mass : ndarray
-        Numpy array of shape (ngals, ) storing black hole mass in units of
-        Msun assuming h=0.7
+        Mass of the black hole in units of Msun
+
+    Notes
+    -----
+    See arXiv:1304.7762, section 6.1 for power-law exponent and normalization
 
     """
     loc = np.log10(bh_mass_from_bulge_mass(bulge_mass))
-    scale = 0.28
+    scale = 0.27
     lg_bhm = jran.normal(ran_key, shape=bulge_mass.shape) * scale + loc
     bh_mass = 10**lg_bhm
 